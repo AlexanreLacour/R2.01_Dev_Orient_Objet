960e474f294b90f5786c6e3e167fe89fcad51b9c
Alexandre Lacour <alexandre.lacour04@gmail.com>
2024-02-13 16:11:25 +0100

    Création de la class ScenarioMailServer

---
+++ TP/TP4/src/ScenarioMailServer.java
@@ -0,0 +1,28 @@
+/**
+ * ScenarioMailServer
+ */
+public class ScenarioMailServer {
+
+    public static void main(String[] args) {
+        testMailServer();
+        testHowManyMailItems();
+        testGetNextMailItems();
+        testPost();
+    }
+
+    private static void testMailServer() {
+        
+    }
+
+    private static void testHowManyMailItems() {
+
+    }
+
+    private static void testGetNextMailItems() {
+
+    }
+
+    private static void testPost() {
+
+    }
+}
