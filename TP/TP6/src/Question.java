58a47c9e243af5a0570ad521b86d26ebd93edb2d
Alexandre Lacour <alexandre.lacour04@gmail.com>
2024-03-12 12:08:50 +0100

    Ajouter des fonctionnalités à la classe Question

--- TP/TP6/src/Question.java
+++ TP/TP6/src/Question.java
@@ -2,5 +2,55 @@
  * Question
  */
 public class Question {
-    
+    private String text;
+    private String answer;
+
+    public Question() {
+        text = "";
+        answer = "";
+    }
+
+    public Question(String text, String answer) {
+        if((text != null) && (answer != null)){
+            this.text = text;
+            this.answer = answer;
+        }else{
+            this.text = "";
+            this.answer = "";
+            System.out.println("Error: text et reponse ne peuvent pas etre null");
+        }
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        if(text!=null){
+            this.text = text;
+        }else{
+            System.out.println("Error: text ne peut pas etre null");
+        }
+        
+    }
+
+    public void setAnswer(String answer) {
+        if(answer!=null){
+            this.answer = answer;
+        }else{
+            System.out.println("Error: answer ne peut pas etre null");
+        }
+    }
+
+    public boolean checkAnswer(String response) {
+        return response.equalsIgnoreCase(answer);
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void display() {
+        System.out.println(text);
+    }
 }
