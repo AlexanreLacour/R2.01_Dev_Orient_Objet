6946404d79f199c9c649cd29a5adb52315202806
Alexandre Lacour <alexandre.lacour04@gmail.com>
2024-03-12 12:41:54 +0100

    Ajouter des choix et afficher la question dans ChoiceQuestion.java

--- TP/TP6/src/question/ChoiceQuestion.java
+++ TP/TP6/src/question/ChoiceQuestion.java
@@ -9,23 +9,36 @@ public class ChoiceQuestion extends Question {
     private ArrayList<String> choices;
 
     public ChoiceQuestion(String text) {
-        super(text, "");
-        choices = new ArrayList<String>();
+        super(text, ""); /* Chainage explicite */
+        this.choices = new ArrayList<String>();
+    }
+
+    public static void main(String[] args) {
+        ChoiceQuestion q = new ChoiceQuestion("Quelle est la couleur du cheval blanc de Henry IV?");
+        q.addChoice("blanc", true);
+        q.addChoice("noir", false);
+        q.display();
+        System.out.println("Reponse: " + q.checkAnswer("1"));
+        System.out.println("Reponse: " + q.checkAnswer("2"));
     }
 
     public void addChoice(String choice, boolean correct) {
-        choices.add(choice);
-        if(correct){
-            String choiceString = "" + choices.size();
-            setAnswer(choiceString);
+        if (choice != null) {
+            this.choices.add(choice);
+            if (correct) {
+                int num = this.choices.size();
+                num += 1;
+                this.setAnswer(String.valueOf(num));
+            }
+        }else{
+            System.out.println("Error: choice ne peut pas etre null");
         }
     }
 
     public void display() {
-        super.display();
+        super.display(); /* Lancement du display hérité */
         for (int i = 0; i < choices.size(); i++) {
-            int choiceNumber = i + 1;
-            System.out.println(choiceNumber + ": " + choices.get(i));
+            System.out.println((i+1) +" : " + this.choices.get(i));
         }
     }
 }
