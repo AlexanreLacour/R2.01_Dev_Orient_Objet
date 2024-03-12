0522dae2248e1720797936f3f9674807a87eff35
Alexandre Lacour <alexandre.lacour04@gmail.com>
2024-03-12 11:53:06 +0100

    Ajouter des méthodes à la classe Quiz et une ligne vide à la classe ChoiceQuestion

--- TP/TP6/src/ChoiceQuestion.java
+++ TP/TP6/src/ChoiceQuestion.java
@@ -3,4 +3,5 @@
  */
 public class ChoiceQuestion extends Question {
 
+    
 }
--- TP/TP6/src/Quiz.java
+++ TP/TP6/src/Quiz.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
+
 /**
  * Quiz
  */
 public class Quiz {
 
+    public Quiz() {
+        
+    }
+
+    public Quiz(ArrayList<Question> list) {
+        
+    }
+
+    public Question pickAtRandom() {
+        return null;
+    }
+
+    public String getQuestionText(int index) {
+        return null;
+    }
+
+    public int getQNumber() {
+        return 0;
+    }
+
+    public void add(Question q) {
+        
+    }
+
+    public void display() {
+        
+    }
 }
