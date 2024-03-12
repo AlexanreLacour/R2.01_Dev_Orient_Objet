8ff3115a72708929ab967cc4e49667319a6ab8c0
Alexandre Lacour <alexandre.lacour04@gmail.com>
2024-03-12 12:18:48 +0100

    Ajouter des choix à la question de choix

--- TP/TP6/src/question/ChoiceQuestion.java
+++ TP/TP6/src/question/ChoiceQuestion.java
@@ -1,8 +1,31 @@
 package question;
+
+import java.util.ArrayList;
+
 /**
  * ChoiceQuestion
  */
 public class ChoiceQuestion extends Question {
+    private ArrayList<String> choices;
 
+    public ChoiceQuestion(String text) {
+        super(text, "");
+        choices = new ArrayList<String>();
+    }
 
+    public void addChoice(String choice, boolean correct) {
+        choices.add(choice);
+        if(correct){
+            String choiceString = "" + choices.size();
+            setAnswer(choiceString);
+        }
+    }
+
+    public void display() {
+        super.display();
+        for (int i = 0; i < choices.size(); i++) {
+            int choiceNumber = i + 1;
+            System.out.println(choiceNumber + ": " + choices.get(i));
+        }
+    }
 }
